421426531236f5b9d29ae16eb36dd924f57002dd
MJ Sandoval <mjsandoval@MJs-MacBook-Pro.fios-router.home>
2018-10-02 20:25:21 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+		cardsInPlay = [];
+	} if (cards[0] === cards[2]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
