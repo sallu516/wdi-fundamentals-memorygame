19cc4c25faa8f976760b7259a36bf325c8643486
MJ Sandoval <mjsandoval@MJs-MacBook-Pro.fios-router.home>
2018-10-03 19:14:44 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,59 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) { 
+    if (cardsInPlay[0].suit === cardsInPlay[1].suit){
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }       
+  } 
+}
 
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var flipCard = function() {
+  var cardId = this.getAttribute('data-id');
+  this.setAttribute('src', cards[cardId].cardImage);
+  
+  console.log("User flipped "+ cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+  
+  cardsInPlay.push(cards[cardId].rank);
+  checkForMatch();  
+}
 
-if (cardsInPlay.length === 2) {
-		cardsInPlay = [];
-	} if (cards[0] === cards[2]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+var createBoard = function() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+  
+    var gameBoard = document.getElementById("game-board");      
+    gameBoard.appendChild(cardElement);
+  }
+}
+
+createBoard();
